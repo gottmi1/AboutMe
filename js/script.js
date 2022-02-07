9e17fe1c06aa48de3727f9b313fbfaa27730e9cb
gottmi1 <91591041+gottmi1@users.noreply.github.com>
2022-02-08 07:02:12 +0900

    project 수정

--- js/script.js
+++ js/script.js
@@ -20,11 +20,29 @@ setTimeout(() => {
   $span[1].style.opacity = 1;
 }, 2500);
 
+const colorChange = (color, delay, textColor = "#eee") => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      $colorBox.style.backgroundColor = color;
+      $span[0].style.color = textColor;
+      $span[1].style.color = textColor;
+      resolve();
+    }, delay);
+  });
+};
+
 setTimeout(() => {
   $header.style.opacity = 1;
-  $colorBox.style.width = "300%";
+  $colorBox.style.width = "500%";
   $span[0].style.color = "#eee";
   $span[1].style.color = "#eee";
+  colorChange("red", 1100)
+    .then(() => colorChange("orange", 1100, "#fff"))
+    .then(() => colorChange("yellow", 1100, "#555"))
+    .then(() => colorChange("green", 1100))
+    .then(() => colorChange("blue", 1100))
+    .then(() => colorChange("#1f45fc", 1100))
+    .then(() => colorChange("indigo", 1100));
 }, 3000);
 
 $list[0].addEventListener("click", () => {
@@ -51,7 +69,7 @@ $list[2].addEventListener("click", () => {
   $box[2].classList.replace("hei-0", "hei-85");
   $box[1].classList.replace("hei-85", "hei-0");
   $box[0].classList.replace("hei-85", "hei-0");
-  document.body.classList.add("over-hid");
+  // document.body.classList.add("over-hid");
   $list[2].classList.add("menu-txt");
   $list[1].classList.remove("menu-txt");
   $list[0].classList.remove("menu-txt");
@@ -61,7 +79,7 @@ $list[3].addEventListener("click", () => {
   $box[1].classList.replace("hei-85", "hei-0");
   $box[2].classList.replace("hei-85", "hei-0");
   $box[0].classList.replace("hei-85", "hei-0");
-  document.body.classList.remove("over-hid");
+  // document.body.classList.remove("over-hid");
   $list[3].classList.add("menu-txt");
   $list[1].classList.remove("menu-txt");
   $list[2].classList.remove("menu-txt");
@@ -69,8 +87,6 @@ $list[3].addEventListener("click", () => {
 });
 
 // --------------------- 메뉴 클릭 했을 때 변하는 동작
-
-// 메인 글자 사진 차례대로 나오는 동작
 
 const $scrollDown = document.querySelector("#scroll-down");
 console.log($scrollDown);
@@ -99,26 +115,26 @@ setInterval(() => {
 
 // click!!
 
-const $toTop = document.querySelector("#toTop");
-const windowHeight = window.innerHeight;
-let heightNum = 0;
+// const $toTop = document.querySelector("#toTop");
+// const windowHeight = window.innerHeight;
+// let heightNum = 0;
 
-$toTop.addEventListener("click", () => {
-  scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-});
+// $toTop.addEventListener("click", () => {
+//   scrollTo({
+//     top: 0,
+//     behavior: "smooth",
+//   });
+// });
 
 //toTop버튼 클릭이벤트
 
 window.addEventListener("wheel", (e) => {
-  console.log(scrollY);
-  if (scrollY >= 400) {
-    $toTop.classList.replace("opa-0", "opa-1");
-  } else {
-    $toTop.classList.replace("opa-1", "opa-0");
-  }
+  // console.log(scrollY);
+  // if (scrollY >= 400) {
+  //   $toTop.classList.replace("opa-0", "opa-1");
+  // } else {
+  //   $toTop.classList.replace("opa-1", "opa-0");
+  // }
 
   // toTop 버튼과 스크롤 다운이미지를 나오고 들어가게 하는 동작
 
@@ -145,10 +161,11 @@ window.addEventListener("wheel", (e) => {
     }
   } else if ($list[2].classList.contains("menu-txt")) {
     if (del > 0) {
+      $scrollDown.style.display = "none";
       $list[2].classList.remove("menu-txt");
       $list[3].classList.add("menu-txt");
       $box[2].classList.replace("hei-85", "hei-0");
-      document.body.classList.remove("over-hid");
+      // document.body.classList.remove("over-hid");
     } else {
       $list[2].classList.remove("menu-txt");
       $list[1].classList.add("menu-txt");
@@ -156,13 +173,34 @@ window.addEventListener("wheel", (e) => {
       $box[2].classList.replace("hei-85", "hei-0");
     }
   } else if ($list[3].classList.contains("menu-txt")) {
-    $scrollDown.style.display = "none";
     if (pageYOffset === 0 && del < 0) {
       $list[3].classList.remove("menu-txt");
       $list[2].classList.add("menu-txt");
       $box[2].classList.replace("hei-0", "hei-85");
-      document.body.classList.add("over-hid");
+      // document.body.classList.add("over-hid");
     }
   }
 });
 //  스크롤로 메뉴 바꾸는 효과주는 동작
+
+const $toyImg = document.querySelector("#toy-img-box");
+const $toyToggle = document.querySelectorAll("#toy-toggle > div");
+
+for (let j = 0; j < $toyToggle.length; j++) {
+  $toyToggle[j].addEventListener("click", (e) => {
+    for (let i = 0; i < $toyToggle.length; i++) {
+      $toyToggle[i].classList.remove("toggle-on");
+    }
+    e.target.classList.add("toggle-on");
+
+    if ($toyToggle[0].classList.contains("toggle-on")) {
+      $toyImg.style.marginLeft = "0%";
+    } else if ($toyToggle[1].classList.contains("toggle-on")) {
+      $toyImg.style.marginLeft = "-100%";
+    } else if ($toyToggle[2].classList.contains("toggle-on")) {
+      $toyImg.style.marginLeft = "-200%";
+    } else if ($toyToggle[3].classList.contains("toggle-on")) {
+      $toyImg.style.marginLeft = "-300%";
+    }
+  });
+}
